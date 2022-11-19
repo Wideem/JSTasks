b8025b3a16a722b26694d57b1919f578d926ca9c
wideem <wideem@gmail.com>
2022-11-19 22:19:18 +0200

    task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    sum() {
+        return this.a + this.b;
+    }
+
+    subtraction(a, b) {
+        return this.a - this.b;
+    }
+
+    multiplication(a, b) {
+        return this.a * this.b
+    }
+    division(a, b) {
+        return this.a / this.b
+    }
+}
+
+const calc = new Calculator(1, 2)
+console.log(calc.sum())
+console.log(calc.subtraction())
