fe5fcb7c9ccafa3bf9a8f680c889d6c232932ca0
wideem <wideem@gmail.com>
2022-11-19 19:12:04 +0200

    added task1

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,27 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+
+document.getElementById("submit-btn").addEventListener("click", clickCall);
+
+
+
+
+function convert() {
+    const kg = document.getElementById("search").value;
+    const lbs = Math.round(kg * 2.2046 * 100) / 100; //Round to two decimal places
+    const g = kg / 0.0010000
+    const oz = Math.round(kg * 35.274 * 100) / 100;
+    return '<p>' + kg + 'kg </p>' + '<p>' + lbs + ' lb </p > ' + '<p>' + g + ' g </p > ' + '<p>' + oz + ' oz</p > ';
+}
+
+function clickCall(event) {
+    event.preventDefault();
+    const output = document.getElementById("output");
+    output.classList.add("headline");
+    const a = document.getElementById("search").value;
+    output.textContent = a;
+    output.innerHTML = convert();
+
+}
