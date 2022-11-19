104fe0d633bec1eeac11802d170bf0ec0a37c112
wideem <wideem@gmail.com>
2022-11-19 22:22:18 +0200

    task 9

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,6 +6,21 @@ title: string
 director: string
 budget: number
 
-Metodas: 
-wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
+Metodas:
+wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+        return this.budget > 100000000
+    }
+}
+
+const movie = new Movie("James Bond", "Keanu", 1000000001)
+console.log(movie.wasExpensive())
